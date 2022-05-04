927b75db49aded21bac1866f786d464932947651
Chris-Styven <104398875+ChrisTheExaminer@users.noreply.github.com>
2022-05-04 18:43:49 +0100

    Rename Walmart Query.sql to Walmart Sales.sql

--- Walmart Sales.sql
+++ Walmart Sales.sql
@@ -9,10 +9,10 @@ Within this file you will find the following fields:\
 Store - the store number
 Date - the week of sales
 Weekly_Sales - sales for the given store
-Holiday_Flag - whether the week is a special holiday week 1 – Holiday week 0 – Non-holiday week
+Holiday_Flag - whether the week is a special holiday week 1 â€“ Holiday week 0 â€“ Non-holiday week
 Temperature - Temperature on the day of sale
 Fuel_Price - Cost of fuel in the region
-CPI – consumer price index
+CPI â€“ consumer price index
 Unemployment - unemployment rate
 Holiday Events\
 Super Bowl: 12-Feb-10, 11-Feb-11, 10-Feb-12, 8-Feb-13\
